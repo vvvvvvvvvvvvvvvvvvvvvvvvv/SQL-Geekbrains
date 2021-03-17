bc5b95fab7d415205e12718a1499f84ecdfc4adf
vvvvvvvvvvvvvvvvvvvvvvvvv <31472677+vvvvvvvvvvvvvvvvvvvvvvvvv@users.noreply.github.com>
2021-03-17 11:08:09 +0300

    Agregation functions

---
+++ lesson4AgregationFunctions/task1.sql
@@ -0,0 +1,12 @@
+drop table if exists users;
+create table users(
+	id serial primary key auto_increment,
+    birthday_at date comment 'Дата рождения',
+    created_at datetime default current_timestamp,
+    updated_at datetime default current_timestamp on update current_timestamp,
+    name varchar(255) comment 'Имя покупателя'
+) comment = 'Покупатели';
+insert into users(id, name, birthday_at) values (null, 'Petr', '1970-01-01');
+insert into users(id, name, birthday_at) values (null, 'Niko', '1980-07-05');
+insert into users(id, name, birthday_at) values (null, 'Kosty', '1990-09-11');
+select FROM_UNIXTIME(AVG(UNIX_TIMESTAMP(birthday_at))) from users;
